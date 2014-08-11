1efb69b02a8d936abff0b28a0dda8de800a2f06d
Eli Jordan <elias.k.jordan@gmail.com>
2014-08-11 11:12:23 +1000

    test

--- project/Build.scala
+++ project/Build.scala
@@ -40,10 +40,10 @@ object Build extends Build {
     TwirlKeys.templateImports += "org.bson.types.ObjectId",
 
     // create source maps so the coffee script can be debugged easily
-    CoffeeScriptKeys.sourceMap := true,
+    //CoffeeScriptKeys.sourceMap := true,
 
     // use node to compile coffeescript
-    JsEngineKeys.engineType := JsEngineKeys.EngineType.Node,
+    //JsEngineKeys.engineType := JsEngineKeys.EngineType.Node,
 
     // download the source
     EclipseKeys.withSource := true
